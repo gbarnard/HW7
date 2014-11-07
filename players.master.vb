7301ece0790b5c095649b33519e83ba191629d2e
Grant Barnard <grant-barnard@uiowa.edu>
2014-11-06 18:31:06 -0600

    added time and routed after delete and insert

--- players.master.vb
+++ players.master.vb
@@ -1,5 +1,10 @@
 ﻿
 Partial Class players
     Inherits System.Web.UI.MasterPage
+
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lbl_time.Text = TimeOfDay.ToShortTimeString
+    End Sub
 End Class
 
